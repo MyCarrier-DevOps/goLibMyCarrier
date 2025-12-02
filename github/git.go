4320f348833f7216ccc2f28a4cfdfc6329d75076
Siva Munukutla <smunukutla@mycarrier.io>
2025-12-02 14:16:42 -0600

    deps: update go-yaml, quic-go, and compress dependencies across multiple modules

--- github/git.go
+++ github/git.go
@@ -11,7 +11,7 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	ghhttp "github.com/go-git/go-git/v5/plumbing/transport/http"
-	"github.com/google/go-github/v73/github"
+	"github.com/google/go-github/v79/github"
 	"golang.org/x/oauth2"
 )
 
